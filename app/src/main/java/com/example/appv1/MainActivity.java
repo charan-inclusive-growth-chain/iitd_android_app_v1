41d53db90189d60d4c42299ac9de1190dba96f63
charan-inclusive-growth-chain <phaneendra@inclusivegrowthchain.com>
2023-10-20 20:02:42 +0530

    Init commit

---
+++ app/src/main/java/com/example/appv1/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.appv1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MaterialToolbar toolbar = findViewById(R.id.topAppBar);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        replaceFragment(new DashboardFrag());
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                drawerLayout.openDrawer(GravityCompat.START);
+
+            }
+        });
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull @org.jetbrains.annotations.NotNull MenuItem item) {
+
+                int id = item.getItemId();
+                item.setChecked(true);
+                drawerLayout.closeDrawer(GravityCompat.START);
+                if (id == R.id.menu_loan) {
+                    replaceFragment(new LoanListFrag());
+                }
+                else if(id == R.id.menu_dashboard) {
+                    replaceFragment(new DashboardFrag());
+                }else if (id == R.id.menu_store) {
+                    replaceFragment(new StoreFrag());
+                } else if (id == R.id.menu_sell) {
+                    Toast.makeText(MainActivity.this, "Sell is Clicked", Toast.LENGTH_SHORT).show();
+                } else if (id == R.id.menu_transaction) {
+                    Toast.makeText(MainActivity.this, "Transaction is Clicked", Toast.LENGTH_SHORT).show();
+                } else if (id == R.id.menu_cropadvisory) {
+                    Toast.makeText(MainActivity.this, "Crop Advisory is Clicked", Toast.LENGTH_SHORT).show();
+                } else if (id == R.id.menu_chatbot) {
+                    Toast.makeText(MainActivity.this, "Chatbot is Clicked", Toast.LENGTH_SHORT).show();
+                } else if (id == R.id.menu_logout) {
+                    Toast.makeText(MainActivity.this, "Logout is Clicked", Toast.LENGTH_SHORT).show();
+                } else if (id == R.id.menu_settings) {
+                    Toast.makeText(MainActivity.this, "Settings is Clicked", Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.menu_profile) {
+                    replaceFragment(new ProfileFrag());
+                }
+
+                return true;
+            }
+        });
+
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/appv1/StoreFrag.java
@@ -0,0 +1,93 @@
+package com.example.appv1;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import android.widget.AdapterView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.Toast;
+
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link StoreFrag#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class StoreFrag extends Fragment {
+
+
+
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public StoreFrag() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment StoreFrag.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static StoreFrag newInstance(String param1, String param2) {
+        StoreFrag fragment = new StoreFrag();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView =  inflater.inflate(R.layout.fragment_store, container, false);
+
+        String[] flowerName = {"Rose","Lotus","Lily","Jasmine",
+                "Tulip","Orchid","Levender","RoseMarry","Sunflower","Carnation"};
+        int[] flowerImages = {R.drawable.apples,R.drawable.brinjals,R.drawable.carrots,R.drawable.figs,R.drawable.guavas,R.drawable.lemons,R.drawable.mangoes,
+                R.drawable.pineapples,R.drawable.strawberries,R.drawable.testimage};
+
+        GridView gridView = rootView.findViewById(R.id.gridView);
+        StoreAdapter storeAdapter = new StoreAdapter(requireActivity(), flowerName, flowerImages);
+        gridView.setAdapter(storeAdapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(requireContext(), "You Clicked on " + flowerName[position], Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
+
+        return rootView;
+    }
+}
