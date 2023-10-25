396842c34a444b01c9baf82ee4badfae3cd6cca9
igcmukta <mukta.chanda@inclusivegrowthchain.com>
2023-10-25 16:58:20 +0530

    initial loading screen

---
+++ app/src/main/java/com/example/appv1/InitialSplashScreenActivity.java
@@ -0,0 +1,133 @@
+package com.example.appv1;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.appv1.MainActivity;
+import com.example.appv1.R;
+
+import java.lang.ref.WeakReference;
+import java.util.Locale;
+
+public class InitialSplashScreenActivity extends AppCompatActivity
+{
+    private boolean languageSelected = false;
+    String selectedLanguage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.initial_splash_activity);
+
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean userLoggedIn = preferences.getBoolean("user_logged_in", false);
+        Log.d("Logged in", String.valueOf(userLoggedIn));
+        String savedLanguage = preferences.getString("selected_language", "");
+
+        if(userLoggedIn) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else {
+
+            if (!savedLanguage.isEmpty()) {
+                setAppLocale(savedLanguage);
+                languageSelected = true;
+                com.example.appv1.Utils.loginUserBasedOnRole(getApplicationContext(), this);
+                //restartApp();
+            } else {
+                showLanguageSelectionDialog();
+            }
+        }
+    }
+
+    private void showLanguageSelectionDialog() {
+        final String[] languages = {"English", "हिंदी"}; // Add more languages if needed
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(InitialSplashScreenActivity.this);
+        builder.setTitle("Select Language");
+        builder.setSingleChoiceItems(languages, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Save the selected language
+                if (which == 0) {
+                    selectedLanguage = "en"; // English
+                } else if (which == 1) {
+                    selectedLanguage = "hi"; // Hindi
+                }
+            }
+        });
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Save the selected language
+                setAppLocale(selectedLanguage);
+                dialog.dismiss();
+
+                // Save the selected language to SharedPreferences
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(InitialSplashScreenActivity.this);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("selected_language", selectedLanguage);
+                editor.apply();
+
+                com.example.appv1.Utils.loginUserBasedOnRole(getApplicationContext(), InitialSplashScreenActivity.this);
+                //restartApp();
+            }
+        });
+
+        builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                // Handle dismissal if needed
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+
+    public void setAppLocale(String languageCode) {
+        Locale locale = new Locale(languageCode);
+        Locale.setDefault(locale);
+
+        Configuration configuration = new Configuration();
+        configuration.locale = locale;
+
+        Resources resources = getBaseContext().getResources();
+        resources.updateConfiguration(configuration, resources.getDisplayMetrics());
+    }
+
+    private void restartApp() {
+        if (languageSelected) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            // ended activity from login
+            finish();
+        } else if (requestCode == 2) {
+            com.example.appv1.Utils.loginUserBasedOnRole(getApplicationContext(), InitialSplashScreenActivity.this);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/appv1/Utils.java
@@ -0,0 +1,41 @@
+package com.example.appv1;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+
+import com.example.appv1.InitialSplashScreenActivity;
+//import com.iitd.login.LoginActivity;
+
+public class Utils
+{
+
+    public static void loginUserBasedOnRole(Context context, Activity parentActivity)
+    {
+        Intent userBasedIntent = new Intent(parentActivity, MainActivity.class);
+
+        //        LoginResponse loginResponse = new Gson().fromJson(PreferenceManager.getDefaultSharedPreferences(context).getString("user", ""), LoginResponse.class);
+        //
+        //        if (Objects.isNull(loginResponse) || !loginResponse.isSuccess()) {
+        //            Intent loginIntent = new Intent(parentActivity, LoginActivity.class);
+        //            parentActivity.startActivityForResult(loginIntent, 1);
+        //        } else {
+        //            //todo based on role should send
+        //            if (Objects.equals(loginResponse.getRole(), "associate")) {
+        //                Intent associateActivityIntent = new Intent(parentActivity, AssociateMainActivity.class);
+        //                parentActivity.startActivityForResult(associateActivityIntent, 2);
+        //            }
+        //            else if (Objects.equals(loginResponse.getRole(), "transporter")) {
+        //                Intent transporterActivityIntent = new Intent(parentActivity, TransporterListActivity.class);
+        //                parentActivity.startActivityForResult(transporterActivityIntent, 2);
+        //            }
+        //
+        //            else if (Objects.equals(loginResponse.getRole(), "security")) {
+        //                Intent transporterActivityIntent = new Intent(parentActivity, SecurityListActivity.class);
+        //                parentActivity.startActivityForResult(transporterActivityIntent, 1);
+        //            }
+        //        }
+
+        parentActivity.startActivityForResult(userBasedIntent, 1);
+    }
+}
