976c2961c21732c97900f51cd7eb01e8e0488920
charan-inclusive-growth-chain <phaneendra@inclusivegrowthchain.com>
2023-11-02 19:57:30 +0530

    Loan Pending, Item integration

---
+++ app/src/main/java/com/example/appv1/LoanPendingAdapter.java
@@ -0,0 +1,83 @@
+package com.example.appv1;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Parcelable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+public class LoanPendingAdapter extends ArrayAdapter<JSONObject> {
+    private final List<JSONObject> loanList;
+    public LoanPendingAdapter(@NonNull Context context, List<JSONObject> loanList) {
+        super(context, R.layout.loan_pending_item, loanList);
+        this.loanList = loanList;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.loan_pending_item, null);
+        }
+
+        // Parse JSON data from loanList and populate your loan_pending_item.xml layout here
+        JSONObject loanObject = loanList.get(position);
+        try {
+            String loanAppDate = loanObject.getString("createdAt").substring(0, 10);
+            String loanAmount = loanObject.getString("requestedAmount");
+            String loanInterestRate = loanObject.getString("intrest") + "%";
+            String windowId = loanObject.getString("loanWindowId");
+            String loanId = loanObject.getString("loanId");
+            String pendingStatus = loanObject.getString("fpoApprovalStatus");
+
+            if(pendingStatus.equals("in-process")) {
+                pendingStatus = "Waiting for FPO Approval."
+            }
+            else {
+                pendingStatus = "Waiting for Samunnati Approval."
+            }
+
+
+
+            // Populate the TextViews in your loan_approved_item layout
+            TextView loanDateTextView = convertView.findViewById(R.id.loan_app_date);
+            TextView loanIdTextView = convertView.findViewById(R.id.loan_id);
+            TextView loanWindowIdTextView = convertView.findViewById(R.id.loan_window_id);
+            TextView pendingStatusTextView = convertView.findViewById(R.id.pending_status);
+            ImageButton info = convertView.findViewById(R.id.infoButton);
+            info.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+//                    JSONObject currentJsonObject = loanList.get(position);
+//                    Intent intent = new Intent(getContext(), LoanDetailApprovedActivity.class);
+//                    intent.putExtra("loanObject", currentJsonObject.toString());
+//                    getContext().startActivity(intent);
+                }
+            });
+
+            //loanIdTextView.setText(loanId);
+            loanDateTextView.setText(loanAppDate);
+            loanIdTextView.setText(loanId);
+            loanWindowIdTextView.setText(windowId);
+            pendingStatusTextView.setText(pendingStatus);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return convertView;
+    }
+
+
+}
--- app/src/main/java/com/example/appv1/LoanPendingTabFrag.java
+++ app/src/main/java/com/example/appv1/LoanPendingTabFrag.java
@@ -2,11 +2,32 @@ package com.example.appv1;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +35,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class LoanPendingTabFrag extends Fragment {
+    private LoanPendingAdapter loanAdapter;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -60,5 +82,65 @@ public class LoanPendingTabFrag extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_loan_pending_tab, container, false);
+    }
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        loadData();
+    }
+
+
+
+    private void loadData() {
+        String url = getContext().getString(R.string.url) + "/loanwindow/loan?type=farmer";
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .header("Authorization", "Bearer " + LoginActivity.getToken(getContext()))
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    String responseBody = response.body().string();
+                    try {
+                        JSONObject jsonResponse = new JSONObject(responseBody);
+                        JSONArray jsonArray = jsonResponse.getJSONArray("data");
+                        Log.d("Loans Data", jsonArray.toString());
+                        requireActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    List<JSONObject> pendingLoans = new ArrayList<>();
+                                    for (int i = 0; i < jsonArray.length(); i++) {
+                                        JSONObject object = jsonArray.getJSONObject(i);
+                                        String status = object.getString("status");
+                                        if (status.equals("in-process")) {
+                                            pendingLoans.add(object);
+                                        }
+                                    }
+
+                                    loanAdapter = new LoanPendingAdapter(getContext(), pendingLoans);
+                                    ListView listView = getView().findViewById(R.id.listview);
+                                    listView.setAdapter(loanAdapter);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+
+
     }
 }
